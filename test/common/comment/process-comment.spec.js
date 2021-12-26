4415348e2f44d319b94de2e69abeeb048f850c96
Alexis <xo@wvvw.me>
2021-12-26 23:58:03 +1030

    chore: fix accidental directory

--- test/common/comment/process-comment.spec.js
+++ test/common/comment/process-comment.spec.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import { processComment } from '../../../../src/common/comment/process-comment.js';
+import { processComment } from '../../src/common/comment/process-comment.js';
 
 test('processes comment', t => {
 	t.is(processComment('This is a basic comment.'), 'This is a basic comment.');
