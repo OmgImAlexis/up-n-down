ac44defe3fe2b1e18fefbe7e0ad77c57423ea431
Alexis <xo@wvvw.me>
2021-12-26 23:55:47 +1030

    chore: add basic test

---
+++ test/common/comment/comment/process-comment.spec.js
@@ -0,0 +1,6 @@
+import test from 'ava';
+import { processComment } from '../../../../src/common/comment/process-comment.js';
+
+test('processes comment', t => {
+	t.is(processComment('This is a basic comment.'), 'This is a basic comment.');
+});
